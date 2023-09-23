76b5a0c4c24257ed0f8a793d8fb7f8e296adaa00
Dr. Nobel Khandaker <nobel@huskers.unl.edu>
2023-09-23 11:28:40 +0800

    Added BFS search - use of deque

---
+++ solution/breadh_first_traversal_pattern.py
@@ -0,0 +1,182 @@
+from collections import deque
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, value: int):
+        self.value = value
+        self.left, self.right = None, None
+        self.next = None
+
+
+def traverse(root: TreeNode) -> List[List[int]]:
+    result = []
+    if not root:
+        return result
+    queue = deque()
+    queue.append(root)
+    while queue:
+        level_size = len(queue)
+        current_level = []
+        for _ in range(level_size):
+            current_node = queue.popleft()
+            current_level.append(current_node.value)
+            if current_node.left:
+                queue.append(current_node.left)
+            if current_node.right:
+                queue.append(current_node.right)
+        result.append(current_level)
+    return result
+
+
+def reverse_traverse(root: TreeNode) -> List[List[int]]:
+    result = deque()
+    if not root:
+        return result
+    queue = deque()
+    queue.append(root)
+    while queue:
+        level_size = len(queue)
+        current_level = []
+        for _ in range(level_size):
+            current_node = queue.popleft()
+            current_level.append(current_node.value)
+            if current_node.left:
+                queue.append(current_node.left)
+            if current_node.right:
+                queue.append(current_node.right)
+        result.appendleft(current_level)
+    return list(result)
+
+
+def zigzag_traverse(root: TreeNode) -> List[List[int]]:
+    result = deque()
+    if not root:
+        return result
+    queue = deque()
+    queue.append(root)
+    left_to_right = True
+    while queue:
+        level_size = len(queue)
+        current_level = deque()
+        for _ in range(level_size):
+            current_node = queue.popleft()
+            if left_to_right:
+                current_level.append(current_node.value)
+            else:
+                current_level.appendleft(current_node.value)
+            if current_node.left:
+                queue.append(current_node.left)
+            if current_node.right:
+                queue.append(current_node.right)
+        result.append(list(current_level))
+        left_to_right = not left_to_right
+    return list(result)
+
+
+def level_average(root: TreeNode) -> List[List[int]]:
+    result = []
+    if not root:
+        return result
+    queue = deque()
+    queue.append(root)
+    while queue:
+        level_size = len(queue)
+        current_level = 0.0
+        for _ in range(level_size):
+            current_node = queue.popleft()
+            current_level += current_node.value
+            if current_node.left:
+                queue.append(current_node.left)
+            if current_node.right:
+                queue.append(current_node.right)
+        result.append(current_level / level_size)
+    return result
+
+
+def min_tree_depth(root: TreeNode) -> int:
+    if not root:
+        return 0
+    queue = deque()
+    queue.append(root)
+    min_depth = 0
+
+    while queue:
+        level_size = len(queue)
+        min_depth += 1
+        for _ in range(level_size):
+            current_node = queue.popleft()
+            if not current_node.left and not current_node.right:
+                return min_depth
+            if current_node.left:
+                queue.append(current_node.left)
+            if current_node.right:
+                queue.append(current_node.right)
+
+
+def find_level_order_successor(root: TreeNode, needle: int) -> int:
+    if not root:
+        return -1
+
+    queue = deque()
+    queue.append(root)
+
+    while queue:
+        level_size = len(queue)
+
+        for _ in range(level_size):
+            current_node = queue.popleft()
+
+            if current_node.value == needle:
+                successor = queue.popleft()
+                return successor.value
+
+            if current_node.left:
+                queue.append(current_node.left)
+
+            if current_node.right:
+                queue.append(current_node.right)
+
+    return -1  # needle not found
+
+
+def connect_next_level_order_siblings(root: TreeNode) -> None:
+    if not root:
+        return
+
+    queue = deque()
+    while queue:
+        level_size = len(queue)
+        previous_node = None
+        for _ in range(level_size):
+            current_node = queue.popleft()
+            if previous_node:
+                previous_node.next = current_node
+            previous_node = current_node
+
+            if current_node.left:
+                queue.append(current_node.left)
+            if current_node.right:
+                queue.append(current_node.right)
+    return
+
+
+def connect_all_level_order_siblings(root:TreeNode)->None:
+    if not root:
+        return
+
+    queue = deque()
+    queue.add(root)
+    previous_node = None
+
+    while queue:
+        current_node = queue.popleft()
+        if previous_node:
+            previous_node.next = current_node
+        previous_node = current_node
+
+        if current_node.left:
+            queue.append(current_node.left)
+
+        if current_node.right:
+            queue.append(current_node.right)
--- test/test_solutions.py
+++ test/test_solutions.py
@@ -12,6 +12,7 @@ from solution.two_pointers_pattern_problems import make_squares
 from solution.two_pointers_pattern_problems import search_triplets
 from solution.two_pointers_pattern_problems import dutch_flag_sort
 from solution.url_shortener import shorten_url
+from solution.breadh_first_traversal_pattern import TreeNode, traverse, reverse_traverse, zigzag_traverse, level_average,min_tree_depth,find_level_order_successor
 import sure
 
 
@@ -88,3 +89,65 @@ def test_search_triplets():
 def test_dutch_flag_sort():
     dutch_flag_sort([1, 0, 2, 1, 0, 2]).should.equal([0, 0, 1, 1, 2, 2])
     dutch_flag_sort([1, 0, 0, 1, 0]).should.equal([0, 0, 0, 1, 1])
+
+
+def test_bfs_level_traversal():
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    traverse(root).should.equal([[12], [7, 1], [9, 10, 5]])
+
+
+def test_bfs_reverse_level_traversal():
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    reverse_traverse(root).should.equal([[9, 10, 5], [7, 1], [12]])
+
+
+def test_bfs_zigzag_level_traversal():
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    root.right.left.left = TreeNode(20)
+    root.right.left.right = TreeNode(17)
+    zigzag_traverse(root).should.equal([[12], [1, 7], [9, 10, 5], [17, 20]])
+
+
+def test_bfs_zigzag_level_traversal():
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.left.right = TreeNode(2)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    level_average(root).should.equal([12.0, 4.0, 6.5])
+
+
+def test_min_tree_depth():
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    min_tree_depth(root).should.equal(2)
+
+
+def test_find_level_order_successor():
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(3)
+    root.left.left = TreeNode(4)
+    root.left.right = TreeNode(5)
+    find_level_order_successor(root, 3).should.equal(4)
+
